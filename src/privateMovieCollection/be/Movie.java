8e7bda1d7f7bf3f867dc3adfbc521e450777c22b
scupak <anto1095@easv365.dk>
2020-01-11 16:26:47 +0100

    stuff

--- src/privateMovieCollection/be/Movie.java
+++ src/privateMovieCollection/be/Movie.java
@@ -47,7 +47,7 @@ public class Movie {
         this.lastview = lastview;
         categoryArray = new ArrayList<>();
         
-        setLastviewTekst(sdf.format(lastview));
+        setLastviewText(sdf.format(lastview));
     }
     
     /**
@@ -200,7 +200,7 @@ public class Movie {
      */
     public void setLastview(Date lastview) {
         this.lastview = lastview;
-        setLastviewTekst(sdf.format(lastview));
+        setLastviewText(sdf.format(lastview));
     }
     /**
      * 
